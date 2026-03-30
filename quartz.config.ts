ecbfe2fe79b249d1a58c90dcfd943ba9c5a0a060
DenisSolicen <DenisSolicen@yandex.ru>
2026-03-30 05:25:11 +0300

    Update

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ru-RU",
-    baseUrl: "denissolicen.github.io/Solicen-Investigation",
+    baseUrl: "https://denissolicen.github.io/Solicen-Investigation",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
